1a2424d7fe8d4bdacd8978f4d7a8fe44231a98ea
Almaz Galimov <almazwork@gmail.com>
2025-04-07 15:58:44 +0500

    Add json db

---
+++ e2_bot/app/data_access/shops_json_db.py
@@ -0,0 +1,37 @@
+import json
+
+from e2_bot.domain.entities import Shop
+from e2_bot.domain.repositories import IShopRepository
+
+
+shops_fake_db = {
+    1: "Кушкина 1",
+    2: "Мюллера 2",
+    3: "Монтова 3",
+    4: "Наполеона 4",
+    5: "Слона 5",
+    6: "Кота 6",
+}
+
+
+class JsonShops (IShopRepository):
+    @staticmethod
+    def get(number: int) -> Shop:
+        with open("shops.json", "r", encoding="utf-8") as f:
+            shops = json.load(f)
+            for shop in shops:
+                if shop["id"] == str(number):
+                    return Shop(
+                        number=number,
+                        name=shop["address"],
+                    )
+
+
+
+# with open("shops.json", "r", encoding="utf-8") as f:
+#     shops = json.load(f)
+#     for shop in shops:
+#         if shop["id"] == number
+#         print(shop)
+#         # if shop["number"] == str(2):
+#         #     print(shop)
--- e2_bot/app/services/shop_service.py
+++ e2_bot/app/services/shop_service.py
@@ -1,4 +1,6 @@
-from e2_bot.app.data_access.shops_fake_db import ShopsFakeDB
+from e2_bot.app.data_access.shops_json_db import JsonShops
 from e2_bot.domain.services import ShopService
 
-service = ShopService(ShopsFakeDB)
+service = ShopService(JsonShops)
+
+print(service.get(1))
