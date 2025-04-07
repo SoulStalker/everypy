49eb9871a9f2e526b02048deb02516f2bde388ce
Almaz Galimov <almazwork@gmail.com>
2025-04-07 16:42:55 +0500

    Menu data from domain commands

---
+++ e2_bot/domain/value_objects/__init__.py
@@ -0,0 +1,3 @@
+from .user_command import Command
+
+__all__ = ['Command']
--- e2_bot/domain/value_objects/user_command.py
+++ e2_bot/domain/value_objects/user_command.py
@@ -5,4 +5,5 @@ class Command(Enum):
     START = "/start"
     HELP = "/help"
     SETTINGS = "/settings"
+    CONTACTS = "/contacts"
     UNCLOSED = "/unclosed"
