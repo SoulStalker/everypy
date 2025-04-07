b85940b4e869f4729bd0f1611625d9acfc27028a
Almaz Galimov <almazwork@gmail.com>
2025-04-07 09:33:47 +0500

    Add main menu

--- e2_bot/bot.py
+++ e2_bot/bot.py
@@ -4,12 +4,14 @@ from aiogram import Bot, Dispatcher
 
 from e2_bot.configs import load_config
 from e2_bot.handlers import router
+from e2_bot.keyboards import set_main_menu
 
 
 async def main():
     config = load_config('.env')
     bot = Bot(token=config.tg_bot.token)
     dp = Dispatcher()
+    await set_main_menu(bot)
 
     dp.include_router(router)
 
---
+++ e2_bot/keyboards/__init__.py
@@ -0,0 +1,3 @@
+from .set_menu import set_main_menu
+
+__all__ = ['set_main_menu']
